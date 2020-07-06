d55041e7df402a77708983c169cc05e674cb53bb
vtquan0213@gmail.com <vtquan0213@gmail.com>
2020-07-06 20:11:25 +0700

    ád

--- model/staff/replyQuestionModel.php
+++ model/staff/replyQuestionModel.php
@@ -4,7 +4,7 @@
     $idStaff = $userObject->id;
     $idquestion = $_POST['id'];
     $content = $_POST['content'];
-    $dateNow = (new \DateTime())->format('H:i:s, d/m/');
+    $dateNow = (new \DateTime())->format('H:i:s, d/m/Y');
     try{     
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $serverUsername, $serverPassword);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
